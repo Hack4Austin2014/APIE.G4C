12d46e05fa76e767a45edc38b5f54416c3e0b33a
leejaew <lee.jaewang@gmail.com>
2014-05-31 11:22:32 -0500

    keep all strings inside single quotes & add new string to i18n

--- Resources/ui/common/FirstView.js
+++ Resources/ui/common/FirstView.js
@@ -10,12 +10,12 @@ function FirstView() {
 	//label using localization-ready strings from <app dir>/i18n/en/strings.xml
 	var cards = Ti.UI.createLabel({
 		//color:'#000000',
-		text: "Flashcards!",
-		backgroundColor: "#F66",
-		//backgroundImage: "/images/bg_old_wood_frame_4_4.png",
+		text: Ti.Locale.getString('title') + '\n\n' + Ti.Locale.getString('touchplay'),
+		backgroundColor: '#F66',
+		//backgroundImage: '/images/bg_old_wood_frame_4_4.png',
 		//backgroundRepeat: true,
-		textAlign: "center",
-		borderRadius: 4,
+		textAlign: 'center',
+		//borderRadius: 4,
 		//top: 100,
 		// left: 10,
 		width: Ti.UI.FILL,
@@ -35,8 +35,8 @@ function FirstView() {
 			var tempData = questions.getData();
 			var data = tempData[Math.floor(Math.random() * tempData.length)];
 			this.text = 'How many syllables?\n\n' + Ti.Locale.getString(data.q);
-			this.backgroundColor = "#777";
-			answer = Ti.Locale.getString(data.a) + "\n\n" + Ti.Locale.getString(data.s);
+			this.backgroundColor = '#777';
+			answer = Ti.Locale.getString(data.a) + '\n\n' + Ti.Locale.getString(data.s);
 			
 			//play audio of the word
 			player = Ti.Media.createSound({url:Ti.Locale.getString(data.v)}); // url:'/db/tts/en/v_01.mp3'
@@ -46,7 +46,7 @@ function FirstView() {
 		} else {
 			//display answer
 			this.text = answer;
-			this.backgroundColor = "#F66";
+			this.backgroundColor = '#F66';
 		}
 	});
 	return self;
